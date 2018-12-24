349d27e5f4f367e8cd34cc5ffcd187cdc3120af7
Yumiko Tezuka <ytezuka5@gmail.com>
2018-12-24 13:12:48 +0900

    ログアウト完成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
 before_action :set_article, only: [:show, :edit, :update, :destroy]
+before_action :authenticate_user!
 	def index
-		@articles = Article.all	
+		#@articles = Article.all
+		@articles = current_user.articles
 	end
 
 	def show
@@ -16,6 +18,7 @@ before_action :set_article, only: [:show, :edit, :update, :destroy]
 
 	def create
 		@article = Article.new(article_params)
+		@article.user = current_user
 		if @article.save
 			redirect_to article_path(@article)
 		else
